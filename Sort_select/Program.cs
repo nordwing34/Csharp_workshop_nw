888ebb83699b21f54fe37bf2fd57abdd1c45d8e1
Влад Нордвинг <vlad@nordwing.ru>
2023-03-25 15:23:30 +0300

    Sort_select 01

--- Sort_select/Program.cs
+++ Sort_select/Program.cs
@@ -1,17 +1,39 @@
-﻿using System;
+﻿// Сортировка выбором
+// https://gb.ru/lessons/316875
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Csharp_workshop_nw
+namespace Sort_select
 {
-	class Program
+	class Sorting
 	{
-		static void Main(string[] args)
+		// Сортировка выбором
+		public static int[] Main(int[] collection)
 		{
-			Console.WriteLine("Привет!");
-			Console.ReadKey();
+			int size = collection.Length;
+			for (int i = 0; i < size - 1; i++) 
+			{
+				int pos = i;
+				for(int j = i+1; j < size; j++)
+				{
+					if (collection[j] < collection[pos]) pos = j;
+				}
+				int temp = collection[i];
+				collection[i] = collection[pos];
+				collection[pos] = temp;
+			}
+			return collection;
 		}
+
+		//int[] arr = new int[] { 9, 6, 0, 5, 7, 3, 1, 2 };
 	}
+	
 }
+
+//int arr = new int[] { 9, 6, 0, 5, 7, 3, 1, 2 };
+//System.Console.WriteLine(string.Join(' ', arr));
